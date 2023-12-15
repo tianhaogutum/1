f8c756baf50bf9406f75aab937c41162699d0888
zhongfang <zhongfang.wang@tum.de>
2023-12-15 20:12:00 +0100

    updated gammacorrect.c

--- Implementierung/gammacorrect.c
+++ Implementierung/gammacorrect.c
@@ -9,7 +9,7 @@
 static _Bool v_set = false;//is V set?
 static int version_number = 0;//version number, default is zero
 static _Bool b_set = false;//is B set?
-static int benchmark_number = -1;//benchmark number
+static int benchmark_number = -1;//benchmark number, should be updated to default value
 static char * input_file_name = NULL;//input file name
 static _Bool o_set = false;//is o set?
 static char * output_file_name = NULL;//output file name
@@ -18,7 +18,6 @@ static float a = 0;//temporary setting, should be initialized as the default a
 static float b = 0;//temporary setting, should be initialized as the default b
 static float c = 0;//temporary setting, should be initialized as the default c
 static _Bool gamma_set = false;//is gamma set?
-static _Bool h_set = false;//is h set?
 
 static const struct option long_options[] = {//long options' table
     {"coeffs", required_argument, 0, 256},
@@ -27,13 +26,14 @@ static const struct option long_options[] = {//long options' table
     {0, 0, 0, 0}
 };
 //function signatures
-void parse_options(int argc, char ** argv);//getopt_long()
-void found_option_V(void);//behaviour if found option '-V'
-void found_option_B(void);//behaviour if found option '-B'
-void found_option_o(void);//behaviour if found option '-o'
-void found_option_h(void);//behaviour if found option '-h' or '--help'
-void found_option_coeffs(void);//behaviour if found option '--coeffs'
-void found_option_gamma(void);//behaviour if found option '--gamma'
+static void parse_options(int argc, char ** argv);//getopt_long()
+static void found_option_V(void);//behaviour if found option '-V'
+static void found_option_B(void);//behaviour if found option '-B'
+static void found_option_o(void);//behaviour if found option '-o'
+static void found_option_h(void);//behaviour if found option '-h' or '--help'
+static void found_option_coeffs(void);//behaviour if found option '--coeffs'
+static void found_option_gamma(void);//behaviour if found option '--gamma'
+static void print_help(void);
 
 int main(int argc, char ** argv){
     parse_options(argc, argv);
@@ -41,7 +41,7 @@ int main(int argc, char ** argv){
     return 0;
 }
 
-void parse_options(int argc, char ** argv){
+static void parse_options(int argc, char ** argv){
     int opt = 0;
     while((opt = getopt_long(argc, argv, "V:B::o:h", long_options, NULL)) != -1){
         switch (opt)
@@ -65,6 +65,8 @@ void parse_options(int argc, char ** argv){
             found_option_gamma();
             break;    
         default:
+            fprintf(stderr, "You give a wrong option or you forget to give argument to an option.\nProgram terminated.\n");
+            exit(EXIT_FAILURE);
             break;
         }
     }
@@ -81,7 +83,7 @@ void parse_options(int argc, char ** argv){
     }
 }
 
-void found_option_V(void){
+static void found_option_V(void){
     if(v_set){
         fprintf(stderr, "Option 'V' is already set, please don't set it twice.\nProgram terminated.\n");
         exit(EXIT_FAILURE);
@@ -94,22 +96,27 @@ void found_option_V(void){
     version_number = tmp;
 }
 
-void found_option_B(void){
+static void found_option_B(void){
 
 }
 
-void found_option_o(void){
+static void found_option_o(void){
 
 }
 
-void found_option_h(void){
+static void found_option_h(void){//help text will be printed, and then exit normally
+    print_help();
+    exit(EXIT_SUCCESS);
+}
+
+static void found_option_coeffs(void){
 
 }
 
-void found_option_coeffs(void){
+static void found_option_gamma(void){
 
 }
 
-void found_option_gamma(void){
-
+static void print_help(void){
+    printf("");
 }
