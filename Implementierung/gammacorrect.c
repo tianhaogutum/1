f2a7546f4f5848e798f32b8fdf2d77a37600d048
zhongfang <zhongfang.wang@tum.de>
2023-12-16 14:18:07 +0100

    function for option o and option gamma implemented

--- Implementierung/gammacorrect.c
+++ Implementierung/gammacorrect.c
@@ -3,9 +3,9 @@
 #define VERSION_NUMBER 3 // suppose we have three versions, should be updated
 
 static _Bool v_set = false;           // is V set?
-static int version_number = 0;        // version number, default is zero
+static int version = 0;               // version number, default is zero
 static _Bool b_set = false;           // is B set?
-static int benchmark_number = -1;     // benchmark number, should be updated to default value
+static int benchmark_number = 100;    // benchmark number, should be updated to default value
 static char *input_file_name = NULL;  // input file name
 static _Bool o_set = false;           // is o set?
 static char *output_file_name = NULL; // output file name
@@ -14,7 +14,8 @@ static float a = 0;                   // temporary setting, should be initialize
 static float b = 0;                   // temporary setting, should be initialized as the default b
 static float c = 0;                   // temporary setting, should be initialized as the default c
 static _Bool gamma_set = false;       // is gamma set?
-static const char *program_path;
+static float gamma = 1;               // temporary setting, should be initialized as the default gamma
+static const char *program_path;      // stores the path of the program
 
 static const struct option long_options[] = { // long options' table
     {"coeffs", required_argument, 0, 256},
@@ -31,13 +32,13 @@ static void found_option_coeffs(void);            // behaviour if found option '
 static void found_option_gamma(void);             // behaviour if found option '--gamma'
 static void print_help(void);
 static void print_usage(void);
-static void exit_failure_with_errmessage(const char *);
+static void exit_failure_with_errmessage(const char *); // note that error message must end with newline, this function will log the error to stderr and print usage, and then exit with failure
 
 int main(int argc, char **argv)
 {
-    program_path = argv[0];
+    program_path = argv[0]; // save program path as global
     parse_options(argc, argv);
-
+    printf("version is %d\nbenchmark_number is %d\ngamma is %f\n", version, benchmark_number, gamma);//for testing
     return 0;
 }
 
@@ -60,25 +61,25 @@ static void parse_options(int argc, char **argv)
         case 'h':
             found_option_h();
             break;
-        case 256:
+        case 256: //--coeffs
             found_option_coeffs();
             break;
-        case 257:
+        case 257: //--gamma
             found_option_gamma();
             break;
-        default:
+        default: // option argument missing or unknown option
             exit_failure_with_errmessage("You give a wrong option or you forget to give argument to an option.\nProgram terminated.\n");
         }
     }
-    if (optind >= argc)
+    if (optind >= argc) // no input file
     {
         exit_failure_with_errmessage("No input file specified.\nProgram terminated.\n");
     }
-    else if (optind == argc - 1)
+    else if (optind == argc - 1) // just one input file
     {
         input_file_name = argv[optind];
     }
-    else
+    else // to many input files
     {
         exit_failure_with_errmessage("More than one input file is given.\nProgram terminated.\n");
     }
@@ -90,12 +91,11 @@ static void found_option_V(void)
     {
         exit_failure_with_errmessage("Option 'V' is already set, please don't set it twice.\nProgram terminated.\n");
     }
-    int tmp = atoi(optarg);
-    if (tmp < 0 || tmp > VERSION_NUMBER - 1)
+    version = strtol(optarg, NULL, 10);
+    if (version < 0 || version > VERSION_NUMBER - 1)
     {
         exit_failure_with_errmessage("The given version number is not provided, provided versions are 0, 1, 2\nProgram terminated.\n"); // should be updated
     }
-    version_number = tmp;
     v_set = true;
 }
 
@@ -107,18 +107,23 @@ static void found_option_B(void)
     }
     if (optarg)
     {
-        int tmp = atoi(optarg);
-        if (tmp < 100)
+        benchmark_number = strtol(optarg, NULL, 10);
+        if (benchmark_number < 100)
         {                                                                                                              // should be updated to guarantee a sufficient workload
             exit_failure_with_errmessage("The number of repetitions cannot be less than 100.\nProgram terminated.\n"); // should be updated accordingly
         }
-        benchmark_number = tmp;
-        b_set = true;
     }
+    b_set = true;
 }
 
 static void found_option_o(void)
 {
+    if (o_set)
+    {
+        exit_failure_with_errmessage("Option 'o' is already set, please don't set it twice.\nProgram terminated.\n");
+    }
+    output_file_name = optarg;
+    o_set = true;
 }
 
 static void found_option_h(void)
@@ -133,6 +138,12 @@ static void found_option_coeffs(void)
 
 static void found_option_gamma(void)
 {
+    if (gamma_set)
+    {
+        exit_failure_with_errmessage("Option 'gamma' is already set, please don't set it twice.\nProgram terminated.\n");
+    }
+    gamma = strtof(optarg, NULL);
+    gamma_set = true;
 }
 
 static void print_help(void)
@@ -146,7 +157,7 @@ static void print_usage(void)
 }
 
 static void exit_failure_with_errmessage(const char *errmessage)
-{ // note that error message must end with newline
+{
     fprintf(stderr, "%s", errmessage);
     print_usage();
     exit(EXIT_FAILURE);
